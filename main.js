375ae71fa28de53d12f0f6c5a3bcd7182f38a679
Abduboriy <aaauzbek@gmail.com>
2020-09-25 18:37:29 -0500

    Login PopUp

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+window.addEventListener('load', function () {
+
+    document.getElementById("show_login").onclick = function()
+    {
+        document.getElementsByClassName("container-login")[0].style.display = "block";
+    }
+
+    document.getElementById("hide_login").onclick = function()
+    {
+        document.getElementsByClassName("container-login")[0].style.display = "none";
+    }
+});
+
 const signup = document.querySelector('.popup');
 
 signup.addEventListener('click', () => {
