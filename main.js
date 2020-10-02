e9f6967f5a40eef61a035977d200ab2a123efe53
Victor Wei <vw1262@gmail.com>
2020-10-02 16:11:08 -0500

    signup form

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 window.addEventListener('load', function () {
+    const signup = document.querySelector('#show_signup');
+    const exit = document.querySelectorAll('.close-btn');
 
-    document.getElementById("show_login").onclick = function()
-    {
+    // show signup form
+    signup.addEventListener('click', () => {
+        document.getElementsByClassName("container-login")[1].style.display = "block";
+    });
+
+    // show login form
+    document.getElementById("show_login").onclick = function() {
         document.getElementsByClassName("container-login")[0].style.display = "block";
     }
 
-    document.getElementById("hide_login").onclick = function()
-    {
-        document.getElementsByClassName("container-login")[0].style.display = "none";
-    }
-});
-
-const signup = document.querySelector('.popup');
-
-signup.addEventListener('click', () => {
-    
+    // exit login or signup form
+    exit.forEach(item => {
+        item.addEventListener('click', () => {
+            item.parentElement.style.display = "none";
+        })
+    })
 });
